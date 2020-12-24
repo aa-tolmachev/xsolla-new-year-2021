51acb8a46ac8440a7a37f0d98a2e02b0f18e6854
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2020-12-24 12:37:38 +0500

    fun commit 8

--- homepage/views.py
+++ homepage/views.py
@@ -8,16 +8,14 @@ def index(request):
     new_employees = ''
     employees = ''
 
-'''
+    '''
     for i in range(len(employees_db)):
         employee_form = (f'<input type="radio" name="employee" required'
                     f' value="{employees_db[i]["name"]}">{employees_db[i]["name"]}')
 
         employee_link = f'<a href="employees/{i}/">Детали</a>'
         employees += f'{employee_form} | {employee_link} <br>'
-'''
-
-
+    '''
 
 
     if request.method == 'POST':
