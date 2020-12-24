f095d0ef2fc07e7d904da64e50f436773a770e22
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2020-12-24 17:41:50 +0500

    fun commit 8

--- employees/views.py
+++ employees/views.py
@@ -16,13 +16,49 @@ def employee_detail(request, pk):
 
 def prev_winners(request):
     prev_winners = ''
+    last_round = 0
+
+    prev_employees_arr = psql_methods.prev_employees_per_round()
+
+    if len(prev_employees_arr) > 0:
+        prev_winners += f'<p  style="font-size:30px;">'
+
+
+        for winner in result['fetch']:
+            prev_w = winner[0]
+            win_round = winner[1]
+            
+            if win_round > last_round:
+                last_round = win_round
+                prev_winners += f'<b>Round {last_round}</b><br><br>'
+            
+
+            if prev_w != 'test':
+                prev_winners += f'<b>{prev_w}</b><br>'
+
+        prev_winners += f'</p>'
+
+
+
+
+
+    context = {
+
+        'prev_winners':prev_winners
+    }
+    return render(request, 'employees/prev_winners.html', context)
+
+
+
+
+def prev_winners_old(request):
+    prev_winners = ''
 
     prev_employees_arr = psql_methods.prev_employees()
 
     if len(prev_employees_arr) > 0:
         
-        
-        prev_winners += f'<p  style="font-size:40px;">'
+        prev_winners += f'<p  style="font-size:30px;">'
         for prev_w in prev_employees_arr:
             if prev_w != 'test':
                 prev_winners += f'<b>{prev_w}</b><br>'
